c2c3d01943a7f73ce4047e8ada2eaa18dfba6ed5
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-10-15 02:56:20 +0200

    Added /api/activeresource endpoint in order to find which resource is active + fix

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ app.get("/", (req, res) => {
   res.send("Hello from Express !");
 });
 
+app.get("/api/activeresource", (req, res) => {
+  const resources = getResources();
+  const activeResource = resources.find(
+    (resource) => resource.status === "active"
+  );
+  res.send(activeResource);
+});
+
 app.get("/api/resources", (req, res) => {
   const resources = getResources();
   res.send(resources);
@@ -31,20 +39,22 @@ app.get("/api/resources/:id", (req, res) => {
 });
 
 app.patch("/api/resources/:id", (req, res) => {
+  console.log("Patch request recieved");
   const resources = getResources();
   const { id } = req.params; // const id = req.params.id -- id is the name of param (:id) given in request
-
-  const index = resources.findIndex((item) => item.id === id);  
+  const index = resources.findIndex((item) => item.id === id);
+  const activeResource = resources.find(
+    (resource) => resource.status === "active"
+  );
   resources[index] = req.body;
 
   // ----- Related to activate function -----
-  const activeResource = resources.find(resource => resource.status === "active")
   if (req.body.status === "active") {
     if (activeResource) {
-      return res.status(422).send("Another resource is active !")
+      return res.status(422).send("Another resource is active !");
     }
-    resource[index].status = "active"
-    resource[index].activationTime = new Date()
+    resources[index].status = "active";
+    resources[index].activationTime = new Date();
   }
 
   fs.writeFile(
@@ -52,8 +62,10 @@ app.patch("/api/resources/:id", (req, res) => {
     JSON.stringify(resources, null, 2), // Saving file with new entry
     (err) => {
       if (err) {
+        console.log("Cannot store data in the file !");
         return res.status(422).send("Cannot store data in the file !");
       }
+      console.log("Data have been PATCHed");
       return res.send("Data have been PATCHed");
     }
   );
